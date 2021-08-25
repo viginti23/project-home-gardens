d8ef32241a941445ed6883c1c7147dd1c745864d
Michal <zarodm@gmail.com>
2021-08-25 15:04:12 +0200

    add reset password functionality

--- HomeGarden/settings.py
+++ HomeGarden/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uaf)dnsnzs#5hv=fx96l@@(y25_niso*h5-nin+3fy%ax7s_-q'
+SECRET_KEY = os.environ.get('HOME_GARDENS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -142,3 +142,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'offerlist'
 LOGIN_URL = '/users/login/'
+
+# SMTP CONFIG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('ZARODM_EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('ZARODM_PASSWORD')
--- users/urls.py
+++ users/urls.py
@@ -6,11 +6,19 @@ urlpatterns = [
     path('register/', user_views.register_user, name='register'),
     path('login/', user_views.login_user, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    # USER PROFILE #
     path('<int:pk>/my_account/', user_views.my_account, name='account'),
     path('<int:pk>/', user_views.profile, name='profile'),
+
+    # PASSWORD RESET URLS #
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='users/account/password_reset.html'), name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='users/account/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/account/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/account/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
